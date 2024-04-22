5abf8cb205515cc2281b1d029c4074e824989649
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-04-22 18:20:01 -0500

    Initial Commit

---
+++ ShoppersStop/ShoppersStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppersStopApp.swift
+//  ShoppersStop
+//
+//  Created by Chetan Dhowlaghar on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppersStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
