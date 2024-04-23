5eb540c3a6aebff67f638bab3bb96c61ca77e0fb
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-04-23 13:31:13 -0600

    working cart functionality

--- ShoppersStop/Home/CartView.swift
+++ ShoppersStop/Home/CartView.swift
@@ -13,14 +13,48 @@ struct CartView: View {
     var body: some View {
         VStack{
             if !viewModel.cartItems.isEmpty {
+                ScrollView{
+                    ForEach(viewModel.cartItems.sorted(by: { $0.key.id < $1.key.id }), id: \.key.id) { (key, value) in
+                        CartItem(cartItem: (key,value))
+                    }
+                }
+                HStack {
+                    Text("Total: \(viewModel.totalPrice)")
+                }
                
-                ForEach(viewModel.cartItems.sorted(by: { $0.key.id < $1.key.id }), id: \.key.id) { (key, value) in
-                    Text("\(key.title): \(value)")
-                            }
             } else {
                 Text("Cart is empty")
             }
         }.navigationTitle("Your Cart")
+    }
+}
+
+struct CartItem: View {
+    @EnvironmentObject var cartViewModel: CartViewModel
+    @State var count = 1
+    
+    var cartItem: (Product, Int)
+    init(cartItem: (Product, Int)) {
+        self.cartItem = cartItem
+        _count = State(initialValue: cartItem.1)
+        
+    }
+    var body: some View {
+        VStack{
+            HStack{
+                Text(cartItem.0.title)
+                
+            }
+            Stepper(value: $count, in: 0...10) {
+                Text("Quantity: \(count)")
+            }.onChange(of: count) { oldValue, newValue in
+                if newValue == 0 {
+                    cartViewModel.removeProduct(product: cartItem.0)
+                } else {
+                    cartViewModel.updateQuantity(product: cartItem.0, quantity: count)
+                }
+            }
+        }
     }
 }
 
--- ShoppersStop/Home/CartViewModel.swift
+++ ShoppersStop/Home/CartViewModel.swift
@@ -10,18 +10,31 @@ import Foundation
 class CartViewModel: ObservableObject {
     
     @Published var cartItems: [Product : Int] = [:]
-    @Published var totalPrice: Double = 0.0
+    @Published var totalPrice: Double = 0.0 
     
     func addToCart(product: Product){
         cartItems[product] = 1
+        calculateTotalCost()
     }
     
     func updateQuantity(product: Product, quantity: Int) {
         cartItems[product] = quantity
+        calculateTotalCost()
     }
     
     func removeProduct(product: Product) {
         cartItems.removeValue(forKey: product)
+        calculateTotalCost()
+    }
+    
+    func calculateTotalCost() {
+        var totalCost = 0
+        for cartItem in cartItems {
+            let quantity = cartItem.value
+            let costPerEach = cartItem.key.price
+            totalCost += costPerEach * quantity
+        }
+        self.totalPrice = Double(totalCost)
     }
     
 }
