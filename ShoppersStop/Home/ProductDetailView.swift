630d5e89e73cacdea86fc1b99a0ad9897d548763
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-04-25 08:46:44 -0500

    product detail ui change

--- ShoppersStop/Home/ProductDetailView.swift
+++ ShoppersStop/Home/ProductDetailView.swift
@@ -14,46 +14,65 @@ struct ProductDetailView: View {
     @State var count = 1
     var body: some View {
         ScrollView(.vertical){
-            VStack(alignment: .leading){
-                Text(product.title)
-                    .font(.title)
-              
-                if let thumbnail = product.thumbnail{
-                    ImageView(imageURLString: thumbnail)
-                        .scaledToFill()
-                        .frame(width: getScreenBounds().width - 40, height: getScreenBounds().width - 40)
-                        .background(Color.orange)
-                        .cornerRadius(10)
-                }
-                
-                HStack{
-                    Text("Price: \(product.price)")
-                    Spacer()
-                    if showStepper{
-                        Stepper(value: $count, in: 0...10) {
-                            Text("\(count)")
-                        }.onChange(of: count) { oldValue, newValue in
-                            if newValue == 0 {
-                                showStepper = false
-                                cartViewModel.removeProduct(product: product)
-                            } else {
-                                cartViewModel.updateQuantity(product: product, quantity: count)
+            VStack(alignment: .leading, spacing: 0){
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                            if let thumbnail = product.thumbnail{
+                                    ImageView(imageURLString: thumbnail)
+                                        .scaledToFill()
+                                        .frame(width: getScreenBounds().width - 40, height: getScreenBounds().width - 100)
+                                        .cornerRadius(10)
                             }
-                        }
-                       
+                            if let images = product.images {
+                                ForEach(images, id: \.self) { image in
+                                    ImageView(imageURLString: image)
+                                        .scaledToFill()
+                                        .frame(width: getScreenBounds().width - 40, height: getScreenBounds().width - 100)
+                                        .cornerRadius(10)
+                                }
+                            }
+                    }
+                    .scrollTargetLayout()
+                }
+                .scrollTargetBehavior(.viewAligned)
+                .contentMargins(16, for: .automatic)
+                .listRowInsets(EdgeInsets())
+             
+                HStack{
+                    Text(product.title)
+                    Spacer()
+                    Text("$\(product.price)")
+                } .font(.title3)
+                    .foregroundStyle(.white)
+                    .bold()
+                    .padding()
+            }.background(Color.orange.gradient)
+            
+            if showStepper{
+                CartStepper(itemCount: $count, range: 0...10)
+                .onChange(of: count) { oldValue, newValue in
+                    if newValue == 0 {
+                        showStepper = false
+                        cartViewModel.removeProduct(product: product)
                     } else {
-                        Button(action: {
-                            cartViewModel.addToCart(product: product)
-                            showStepper.toggle()
-                        }, label: {
-                            Text("Add to cart")
-                                
-                        }).buttonBorderShape(.capsule)
+                        cartViewModel.updateQuantity(product: product, quantity: count)
                     }
                 }
-
+               
+            } else {
+                Button(action: {
+                    cartViewModel.addToCart(product: product)
+                    showStepper.toggle()
+                }, label: {
+                    Text("Add to cart")
+                        .bold()
+                        .padding(8)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                })
             }
-        }.padding()
+        }
            
                 .navigationBarTitleDisplayMode(.inline)
                 .navigationBarItems(trailing:
@@ -67,3 +86,32 @@ struct ProductDetailView: View {
 
 
 
+struct CartStepper: View {
+    @Binding var itemCount: Int
+    let range: ClosedRange<Int>
+
+    var body: some View {
+        HStack {
+            Button(action: {
+                if self.itemCount > self.range.lowerBound {
+                    self.itemCount -= 1
+                }
+            }) {
+                Image(systemName: "minus")
+            }
+            .foregroundColor(self.itemCount > self.range.lowerBound ? .orange : .gray)
+            
+            Text("\(itemCount)")
+                .bold()
+            
+            Button(action: {
+                if self.itemCount < self.range.upperBound {
+                    self.itemCount += 1
+                }
+            }) {
+                Image(systemName: "plus")
+            }
+            .foregroundColor(self.itemCount < self.range.upperBound ? .orange : .gray)
+        }
+    }
+}
